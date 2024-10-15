4775db7cf4f7f93b19602e130ba962435c4be2ee
Fan <59925301+DyFan777@users.noreply.github.com>
2024-10-15 13:03:30 -0500

    UPdated loss
    Only deal with zeros

--- utils/loss_calculator.py
+++ utils/loss_calculator.py
@@ -13,11 +13,25 @@ def calculate_loss( mask_pred, diffusion_pred, mask_true,diffusion_true ):
     threshold = 0.5
     binary_mask = (mask_pred_prob >= threshold).float() 
     results_pred = binary_mask * diffusion_pred
+    
     results_true = mask_true * diffusion_true
     results_true = results_true.unsqueeze(1)  # This will add the channel dimension back if needed
 
     loss_mask = F.binary_cross_entropy(mask_pred_prob.squeeze(1), mask_true.float())
-    loss_diffusion = F.mse_loss(results_pred, results_true)
+
+    # Only calculate the MSE based on the non-zero values: 
+    results_true_list = results_true.flatten()
+    results_pred_list = results_pred.flatten()
+    result_combine = [[a,b] for a, b in zip(results_true_list,results_pred_list) if a * b != 0]
+
+    if len(result_combine) == 0:
+        # If there are no non-zero pairs, set diffusion loss to 0
+        loss_diffusion = torch.tensor(0.0, device=mask_pred.device, requires_grad=True)
+    else:
+        results_true_nonzero = torch.stack([x[0] for x in result_combine]).to(mask_pred.device)
+        results_pred_nonzero = torch.stack([x[1] for x in result_combine]).to(mask_pred.device)
+
+        loss_diffusion = F.mse_loss(results_true_nonzero, results_pred_nonzero)
 
     return loss_mask, loss_diffusion
 
